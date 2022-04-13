d1d20d9d29c0b371a70494d006293be6dc17dc1f
dmars <dmars@cnm.edu>
2022-04-13 15:48:01 -0400

    imported source via variable name planted the variable

--- frontEnd/src/ui/AudioPlayer.js
+++ frontEnd/src/ui/AudioPlayer.js
@@ -7,13 +7,19 @@ import {BsArrowBarRight} from "react-icons/bs"
 import {BsPlayFill} from "react-icons/bs"
 import {BsPauseFill} from "react-icons/bs"
 import ReactAudioPlayer from 'react-audio-player';
-import audio from "../audioSamples/Foreign.mp3"
+import song from "../Foreign.mp3"
+
+
 
 
 export const AudioPlayer = () => {
+    const sound = new Audio('Foreign.mp3')
+
    const [isPlaying, setIsPlaying] = useState(false);
    const [duration, setDuration] = useState(0);
    const [currenTime, setCurrentTime] = useState(0)
+
+
 
 
    // references reference our audio component and progress bar
@@ -74,7 +80,7 @@ changePlayerCurrentTime();
 
 
 
-    <audio ref={audioPlayer} src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/af52a99b-88c0-4638-b120-d46e142d06d3/audio/500344fb-2e2b-48af-be86-af6ac341a6da/default_tc.mp3" preload="metadata"> </audio>
+    <audio ref={audioPlayer} src={song} preload="metadata"> </audio>
     <button className={style.forwardBackwards}> <BsArrowBarLeft/> 30s</button>
     <button onClick={togglePlayPause} className={style.playPause}>
         {isPlaying ? <BsPauseFill/> : <BsPlayFill/>}
@@ -94,6 +100,12 @@ changePlayerCurrentTime();
                 </div>
 
             </Container>
+
+
+
+
+
+
 
 
         </>
